473e9cd62e8c8ccae4dd287c2ce1af7a48fbafb3
YOFCit <ityofc@gmail.com>
2025-09-08 09:53:21 -0600

    Update TsheatingController.php

--- app/Http/Controllers/TsheatingController.php
+++ app/Http/Controllers/TsheatingController.php
@@ -8,11 +8,11 @@ class TsheatingController extends Controller
 {
   public function index()
   {
-    return view('Layouts.Lsheating', ['editable' => false], ['tipod' => 'text']);
+    return view('layouts.Lsheating', ['editable' => false], ['tipod' => 'text']);
   }
 
   public function indexAdmin()
   {
-    return view('Layouts.Lsheating', ['editable' => true], ['tipod' => 'text']);
+    return view('layouts.Lsheating', ['editable' => true], ['tipod' => 'text']);
   }
 }
