19f4dc5caf6a8837a640ae2018348f53fdbbd2ea
YOFCit <ityofc@gmail.com>
2025-09-08 10:03:59 -0600

    Update TsheatingComponent.php

--- app/Livewire/TsheatingComponent.php
+++ app/Livewire/TsheatingComponent.php
@@ -3,7 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use App\Models\Tsheating;
+use App\Models\tsheating;
 use Illuminate\Support\Carbon;
 use App\Exports\TsheatingExport;
 use Maatwebsite\Excel\Facades\Excel;
