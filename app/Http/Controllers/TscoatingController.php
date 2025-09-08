c3a6f97e35a1c51c3ac1f68da211d22f70a26600
YOFCit <ityofc@gmail.com>
2025-09-08 09:53:11 -0600

    Update TscoatingController.php

--- app/Http/Controllers/TscoatingController.php
+++ app/Http/Controllers/TscoatingController.php
@@ -8,11 +8,11 @@ class TscoatingController extends Controller
 {
   public function index()
   {
-    return view('Layouts.Lscoating', ['editable' => false], ['tipod' => 'text']);
+    return view('layouts.Lscoating', ['editable' => false], ['tipod' => 'text']);
   }
 
   public function indexAdmin()
   {
-    return view('Layouts.Lscoating', ['editable' => true], ['tipod' => 'text']);
+    return view('layouts.Lscoating', ['editable' => true], ['tipod' => 'text']);
   }
 }
